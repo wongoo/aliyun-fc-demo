2a3500fe348746a7cda5999b297846206f105127
wongoo <wongoo@apache.org>
2022-01-26 17:35:55 +0800

    fix check

--- mq_http_publisher/mq_http_publisher.go
+++ mq_http_publisher/mq_http_publisher.go
@@ -1,10 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mq_http_publisher
 
-import "github.com/aliyunmq/mq-http-go-sdk"
+import mq_http_sdk "github.com/aliyunmq/mq-http-go-sdk"
 
 func Publish(endpoint, accessId, accessSecret, instanceId, topic, messageKey,
 	tag, body string, properties map[string]string) (interface{}, error) {
-
 	client := mq_http_sdk.NewAliyunMQClient(endpoint, accessId, accessSecret, "")
 
 	mqProducer := client.GetProducer(instanceId, topic)
